8f7a523df2ec18d5ad34484cd0ba7e11c6a016db
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-07 14:32:54 +0300

    Three ways for create object

---
+++ index.js
@@ -0,0 +1,14 @@
+//! Object
+//!How create object!
+
+//? 1
+//* literal
+// const cat1 = {};
+
+//? 2
+//* constructor
+// const cat2 = new Object();
+
+//? 3
+//* constructor
+// const cat3 = Object();
